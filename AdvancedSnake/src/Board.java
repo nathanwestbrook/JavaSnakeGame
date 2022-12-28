fa3b83cfee6397cf12a94f3bc557fad670a24772
nathanwestbrook <99312310+nathanwestbrook@users.noreply.github.com>
2022-12-28 17:08:48 -0500

    Add files via upload

---
+++ AdvancedSnake/src/Board.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class Board extends JFrame {
+
+    Board(){
+
+        this.add(new Panel());
+        this.setTitle("Snake Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
